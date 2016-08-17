341db19ca4395fa96c9e19c010ad969d5fc59bee
Anthony Tippett <atippett@gmail.com>
2016-08-17 14:57:19 -0700

    test

--- watchingyou.sh
+++ watchingyou.sh
@@ -1,2 +1,4 @@
 #!env bash
+# example
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/atippett/jokes/master/watchingyou.sh)"
 echo "* */10 * * * say -v whisper 'I am watching you'" |crontab
